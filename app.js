18230633aab1032cf0b1a1a3c25438fd9fe7e1b0
Bernardo Angel <123611473+angel010896@users.noreply.github.com>
2025-02-07 11:46:54 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,45 @@
+let numeroAleatorio = prompt('Del numero 1 hasta que otro numero te gustaria jugar ?');
+let numeroSecreto = Math.floor(Math.random()*(numeroAleatorio))+1;
+let numeroUsuario = 0;
+let intentos = 1;
+//let palabraVeces = 'vez';
+let maximosIntentos = prompt('Cual es el numero maximo de intentos que quieres tener?');
+console.log(numeroSecreto);
+while( numeroUsuario != numeroSecreto){
+    numeroUsuario = parseInt(prompt(` Me indicas un numero entre 1 y ${numeroAleatorio}:`));
+    //pide un numero al usuario y lo guarda en la variable numeroUsuario, parseInt convierte el string (tipo de dato) en numero
+
+console.log(typeof(numeroUsuario));//typeof nos dice el tipo de dato
+ console.log(numeroUsuario);//muestra en consola el numero
+
+ if (numeroUsuario == numeroSecreto) {
+      //alert('felicidades adivinaste el numero : ' + numeroSecreto);
+     //manda la alerta con el texto de que adivino el numero
+     alert(`Felicidades, el numero era ${numeroSecreto} lo hiciste en ${intentos} ${intentos == 1 ? 'vez' : 'veces' }`) // otra forma de hacerlo con comillas invertidas y ${} para mostrar el valor de una variable
+ }
+   else {
+     if(numeroUsuario > numeroSecreto){
+         alert(`El numero es menor `);
+     }
+     else {
+         alert('El numero es mayor');
+   }
+   // alert(' no era ese numero lo siento');
+ };
+ /*let edad = 18;
+ let edadUsuario = prompt('cual es tu edad');
+ if(edadUsuario == edad || edadUsuario > edad ){
+     alert(`eres mayor de edad, tu edad es ${edadUsuario}`);
+ }
+ else{
+     alert(`eres menor de edad`);
+ }*/
+   // intentos = intentos + 1;
+   intentos++;
+    palabraVeces = 'veces';
+    if(intentos > maximosIntentos)
+    {
+        alert(`llegaste al numero maximo de intentos ${maximosIntentos}`);
+        break;    
+    }
+}
